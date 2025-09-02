9f8235aba5be5b1eba47629c8a0d62fd716bae79
Jordan Tobin <jordantobin123@hotmail.com>
2025-09-02 15:08:45 -0700

    Menus functional.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,16 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        // Simply use scene manager to load the next scene in the build sequence.
+        // Note: Scenes must be added to build settings. 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT APPLICATION\n");
+        // The application won't quit inside of unity editor.
+        // Hence the debug log above. 
+        Application.Quit(); 
     }
 }
