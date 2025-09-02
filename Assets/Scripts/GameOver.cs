9f61cc126dc97dd764ea66fd338b4ef91926b3ec
Jordan Tobin <jordantobin123@hotmail.com>
2025-09-02 15:56:36 -0700

    game over menu functional.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        // Load the main game.  
+        SceneManager.LoadScene("_Scene_1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT APPLICATION\n");
+        // The application won't quit inside of unity editor.
+        // Hence the debug log above. 
+        Application.Quit(); 
+    }
+}
